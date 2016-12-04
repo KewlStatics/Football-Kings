46ad65e6501660a24e2ce9690208e2ebbaf67934
iAlain <psfangoyal@gmail.com>
2016-12-04 17:12:09 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,6 @@
 function myLogin() {
     var person = prompt("Please enter your name", "Username");
+    if(confirm("Do u want to continue the game as " + person + "?")) {
+    window.location.href = "home.js"
+   }
     }
