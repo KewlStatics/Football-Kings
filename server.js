20899d4f0d24b701867fbbd88b8d776da38875f9
HoeenHero <HoeenCoder@users.noreply.github.com>
2016-12-25 09:11:42 -0500

    use proper directory setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 
-app.use(express.static(__dirname + 'Index.html'));
+app.use(express.static(__dirname + '/index.html'));
 
 http.listen(process.env.PORT || 3000, () => {
         console.log("This app is now running");
